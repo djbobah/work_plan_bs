06b4662c03756a25262ec28e04b28791c4a2b76d
Vladimir Kirpichev <42208172+djbobah@users.noreply.github.com>
2022-11-03 17:32:49 +0300

    addModal

--- client/src/components/ModalAdd.jsx
+++ client/src/components/ModalAdd.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Modal, Button, InputGroup, Form } from "react-bootstrap";
+import { Modal, Button, InputGroup, Form, Row, Col } from "react-bootstrap";
 
 const ModalAdd = ({ show, onShow, onClose, title }) => {
   // const [show, setShow] = useState(false);
@@ -14,9 +14,63 @@ const ModalAdd = ({ show, onShow, onClose, title }) => {
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Введите дату планируемой работы</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              controlId="exampleFormControlInput1"
+            >
+              <Form.Label column sm="8">
+                Введите дату планируемой работы
+              </Form.Label>
+              <Col sm="4">
+                <Form.Control type="date" placeholder="name@example.com" />
+              </Col>
+            </Form.Group>
+
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" controlId="formGridAddress1">
+              <Form.Label>Address</Form.Label>
+              <Form.Control placeholder="1234 Main St" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formGridAddress2">
+              <Form.Label>Address 2</Form.Label>
+              <Form.Control placeholder="Apartment, studio, or floor" />
+            </Form.Group>
+
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridCity">
+                <Form.Label>City</Form.Label>
+                <Form.Control />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>State</Form.Label>
+                <Form.Select defaultValue="Choose...">
+                  <option>Choose...</option>
+                  <option>...</option>
+                </Form.Select>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridZip">
+                <Form.Label>Zip</Form.Label>
+                <Form.Control />
+              </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" id="formGridCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
           </Form>
         </Modal.Body>
